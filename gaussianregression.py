4c1cc1994550327086611d1e8ee2ef8d8c9ba423
Robert Forrest <robertforrest@live.com>
2019-05-16 12:12:00 +0100

    Fixed bug with noisy sampling

--- gaussianregression.py
+++ gaussianregression.py
@@ -6,44 +6,56 @@ import shutil
 shutil.rmtree('img')
 os.mkdir('img')
 
+
 def f(x):
-    return np.sin(x) + np.cos(np.sqrt(3)*x)
+  return np.sin(x) + np.cos(np.sqrt(3)*x)
 
 def k(a,b):
-  params = [1.0,1.0]
-  
-  return (params[0]**2)*np.exp(-((a-b)**2) /(2*params[1]**2))
+  param = 1.0
+  return np.exp(-0.5*((a-b)/param)**2)
 
-x = np.linspace(0,20,400,dtype=float)
+def kroneker(i,j):
+  if(i==j):
+    return 1
+  else: 
+    return 0
+
+x = np.linspace(0,20,400)
 y = f(x)
+
+noiseSD = 0.1
+noiseVar = noiseSD**2
 
 limits=[int(0.20*len(x)),int(0.80*len(x))]
 
 x_interval = x[limits[0]:limits[1]]
 
 numSamples = 0
-varianceThreshold = 0.0001
+varianceThreshold = 0.001
 averageVarianceHistory=[]
 maxVarianceHistory=[]
 maxVariance=1000000
 x_samples = []
 y_samples = []
+sampleCoverage = []
+dpi = 75
 convertCommand = "convert -delay 40 -loop 0 "
-while maxVariance>varianceThreshold:
+
+iterations= 0
+maxIterations = 50
+
+while maxVariance>varianceThreshold and iterations<maxIterations:
   
-  if(len(x_samples)>1):
-    index = y_variance.index(maxVariance)
-  else:
-    index = np.random.random_integers(limits[0],limits[1])
-
-  x_samples.append(x[index])
-  y_samples.append(y[index])
-
+  if(iterations>0):
+    index = y_variance[limits[0]:limits[1]].index(maxVariance) + limits[0]
+    x_samples.append(x[index])
+    y_samples.append(y[index]+np.random.normal(scale=noiseSD))
+ 
   covariance_matrix = np.zeros([len(x_samples),len(x_samples)])
 
   for i in range(len(x_samples)):
     for j in range(len(x_samples)):
-      covariance_matrix[i][j] = k(x_samples[i],x_samples[j])
+      covariance_matrix[i][j] = k(x_samples[i],x_samples[j]) + (noiseVar)*kroneker(i,j)
 
   try:
     inv_covariance_matrix = np.linalg.inv(covariance_matrix)
@@ -73,27 +85,41 @@ while maxVariance>varianceThreshold:
       upperBound.append(prediction+2*np.sqrt(variance))
 
     maxVariance = np.max(y_variance[limits[0]:limits[1]])
-    averageVarianceHistory.append(np.mean(y_variance[limits[0]:limits[1]]))
+    averageVarianceHistory.append(np.mean(y_variance[limits[0]:limits[1]])*100)
     maxVarianceHistory.append(maxVariance)
 
+    coverage = 100*len(x_samples)/float(len(x))
+    sampleCoverage.append(coverage)
+
     plt.clf()
+    plt.style.use('seaborn-white')
+    plt.figure(figsize=(8,5.333))
     plt.ylim(-2.5,2.5)
     plt.axvline(x=x_interval[0],linestyle='--',color='k')
     plt.axvline(x=x_interval[-1],linestyle='--',color='k')
-    plt.plot(x, y,'r',label='true')
-    plt.plot(x_samples,y_samples,'ko',label="training set",markersize=4)
-    plt.plot(x, y_predictions,label='prediction')
+    plt.xlabel(r'$x$')
+    plt.ylabel(r'$y$')
     plt.fill_between(x, lowerBound, upperBound,color='gray',alpha=0.2,label='uncertainty')
-    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15),ncol=4, fancybox=True)
-    plt.savefig('img/'+str(numSamples)+'.png')
+    plt.plot(x, y,'r',label='true')
+    plt.plot(x, y_predictions,label='prediction')
+    plt.plot(x_samples,y_samples,'ko',label="training set",markersize=4)
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.12),ncol=4, fancybox=True)
+    plt.savefig('img/'+str(iterations)+'.png',bbox_inches='tight',dpi=dpi)
+    plt.close()
+    convertCommand+=" img/"+str(iterations)+".png"
 
-    convertCommand+=" img/"+str(numSamples)+".png"
+  iterations+=1
 
 plt.clf()
+plt.style.use('seaborn-white')
+plt.figure(figsize=(6,4)) 
+plt.grid(True,which="both")
+plt.ylabel("Average Uncertainty (%)")
+plt.xlabel("Training Set Size")
 plt.plot(averageVarianceHistory,label='average uncertainty')
-plt.plot(maxVarianceHistory,label='max uncertainty')
-plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15),ncol=2, fancybox=True)
-plt.savefig('img/variance.png')
+# plt.plot(maxVarianceHistory,label='max uncertainty')
+# plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1),ncol=2, fancybox=True)
+plt.savefig('img/uncertainty.png',bbox_inches='tight',dpi=dpi)
 
 convertCommand+=" img/regression.gif"
-subprocess.call(convertCommand,shell=True)
+# subprocess.call(convertCommand,shell=True)
